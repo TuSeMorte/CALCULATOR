8d2c6eb4319467fb7fda989de82f0df3db12158b
TuSeMorte <TuSeMorte@gmail.com>
2019-08-30 10:22:29 -0400

    added code final code for digits

--- js/calculator.js
+++ js/calculator.js
@@ -3,7 +3,8 @@ let globalVariables = {
     operatorState: false,
     operator: "+",
     number: 0,
-    firstNumber: 0
+    firstNumber: 0,
+    dividedByZero: false
 }
 //Update Global before operate function
 function updateGlobal(operator) {
@@ -57,6 +58,10 @@ function performOperation(operator,globalNum,userNum ){
         case "X":
             return globalNum * userNum;
         default:
+            if(userNum === 0) {
+                globalVariables.dividedByZero = true;
+                return "Dividing By Zero is Not Allowed!";
+            }
             return globalNum / userNum;
     }
 }
@@ -69,10 +74,14 @@ function digitClick(num) {
         currentDisplay.textContent = ""; 
         globalVariables.operatorState = false;
     }
-    if(currentDisplay.textContent.length <=9){
+    if(num === '.' && currentDisplay.textContent.includes('.')){
+        alert('decimal already')
+        return;
+    }
+    if(currentDisplay.textContent.length <=20){
     currentDisplay.textContent += num;
     }else{
-        alert("This calculator is limited to 9 digits");
+        alert("This calculator is limited to 20 digits");
     }
 }
 //backSpace Click
@@ -86,12 +95,95 @@ function backSpace(){
         currentDisplay.textContent =  currentDisplay.textContent.substr(0, currentDisplay.textContent.length-1);
     }
 }
+//Keypress
+function processKeyPress(key){
+    switch (key){
+        case '0':
+            digitClick('0');
+            break;
+        case '1':
+            digitClick('1');
+            break;
+        case '2':
+            digitClick('2');
+            break;
+        case '3':
+            digitClick('3');
+            break;
+        case '4':
+            digitClick('4');
+            break;
+        case '5':
+            digitClick('5');
+            break;
+        case '6':
+            digitClick('6');
+            break;
+        case '7':
+            digitClick('7');
+            break;
+        case '8':
+            digitClick('8');
+            break;
+        case '9':
+            digitClick('9');
+            break;
+        case 'backspace':
+            backSpace();
+            break;
+        case 'delete':
+            backSpace();
+            break;
+        case '+':
+            updateGlobal('+');
+            operate();
+            globalVariables.operator = '+';
+            break;
+        case '-':
+            updateGlobal('-');
+            operate();
+            globalVariables.operator = '-';
+            break;
+        case '*':
+            updateGlobal('X');
+            operate();
+            globalVariables.operator = 'X';
+            break;
+        
+        case '=':
+            Equal();
+            break;
+        default:
+            updateGlobal('/');
+            operate();
+            globalVariables.operator = '/';
+            break;
+    }
+}
+//Equals
+function Equal(){
+    let previousDisplay = document.getElementById("history");
+    let currentDisplay = document.getElementById("currentNum");
+    let currentNumberDisplay = Number(currentDisplay.textContent);
+    let currentNumberGlobal = globalVariables.number;
+    globalVariables.firstNumber = currentNumberDisplay;
+
+    let operationResult = performOperation(globalVariables.operator, currentNumberGlobal, globalVariables.firstNumber);
+
+    previousDisplay.textContent = "";
+    currentDisplay.textContent = operationResult;
+}
 
 //Button Clicks
 let digits = document.querySelectorAll('.digit');
 digits.forEach(function(digit) {
   digit.addEventListener('click', function() {
       console.log(digit.innerHTML);
+      if(globalVariables.dividedByZero === true) {
+            globalVariables.dividedByZero = false;
+          AC();
+        
+      }
       digitClick(digit.innerHTML);
   });
 });
@@ -113,6 +205,7 @@ decBut.addEventListener('click', function() {
 let eqBut = document.getElementById('Equal');
 eqBut.addEventListener('click', function() {
       console.log(eqBut.innerHTML);
+        Equal();
   });
 //clear 
 let clrBut = document.getElementById('AC');
@@ -127,3 +220,20 @@ bsBut.addEventListener('click', function() {
       backSpace();
 //      //addBut(addBut.innerHTML);
   });
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    document.addEventListener('keydown', event => {
+        const charList = '.*/-=+0123456789';
+        const key = event.key.toLowerCase();
+
+        // we are only interested in alphanumeric keys
+        if (charList.indexOf(key) !== -1 || key === 'backspace' | key === 'delete'){
+            processKeyPress(key);
+            console.log(key);
+         }else {
+            return;
+    }
+    });
+});
